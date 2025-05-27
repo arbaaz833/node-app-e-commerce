90ed555e164f48189d845a16bc12c05ba57a7e0b
arbaaz833 <arbaazali031@gmail.com>
2025-05-28 02:04:59 +0500

    uploads directory handled

--- app.ts
+++ app.ts
@@ -3,7 +3,7 @@ import cors from 'cors';
 import productsRouter from './products/products.router';
 import path from 'path';
 
-const assetsDir = path.join(__dirname,'uploads');
+const assetsDir = path.join('uploads');
 export const app = express();
 
 app.use(express.json());
